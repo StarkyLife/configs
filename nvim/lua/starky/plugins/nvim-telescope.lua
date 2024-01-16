6cd4e227914d7f749e4d2e29f225ec5eb28508bf
Ilshat Ismagilov <IsmagilovIF@suek.ru>
2024-01-16 11:19:40 +0300

    better telescope mapping

--- nvim/lua/starky/plugins/nvim-telescope.lua
+++ nvim/lua/starky/plugins/nvim-telescope.lua
@@ -6,7 +6,23 @@ return {
 		"nvim-tree/nvim-web-devicons"
 	},
 	config = function ()
+    local telescope = require("telescope")
 		local builtin = require('telescope.builtin')
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          },
+          n = {
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          },
+        },
+      },
+    })
+
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
 		vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Find in git files" })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find in buffers" })
