30af38eb5becacca87355c0c62e2cded339b4040
Ilshat Ismagilov <ilshat.ismagilov@reksoft.com>
2026-07-10 10:39:49 +0300

    learning

--- nvim-new/lua/plugins/completion.lua
+++ nvim-new/lua/plugins/completion.lua
@@ -1,12 +1,35 @@
 return {
-  {
-    "saghen/blink.cmp",
-    version = "1.*",
-    dependencies = { "rafamadriz/friendly-snippets" },
-    config = function ()
-      local blink = require("blink.cmp")
-      blink.setup({ keymap = { preset = "enter" } })
-      vim.lsp.config("*", { capabilities = blink.get_lsp_capabilities() })
-    end
-  }
+	{
+		"saghen/blink.cmp",
+		version = "1.*",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		config = function()
+			local blink = require("blink.cmp")
+			blink.setup({
+				keymap = { preset = "enter" },
+				completion = {
+					menu = {
+						draw = {
+							columns = { { "kind_icon", "label", "label_description", gap = 1 } },
+							components = {
+								label_description = {
+									width = { max = 30 },
+									text = function(ctx)
+										return ctx.label_description ~= "" and ctx.label_description
+											or (ctx.item.detail or "")
+									end,
+									highlight = "BlinkCmpLabelDescription",
+								},
+							},
+						},
+					},
+					documentation = {
+						auto_show = true,
+						auto_show_delay_ms = 200,
+					},
+				},
+			})
+			vim.lsp.config("*", { capabilities = blink.get_lsp_capabilities() })
+		end,
+	},
 }
--- nvim-new/lua/plugins/ui.lua
+++ nvim-new/lua/plugins/ui.lua
@@ -1,21 +1,28 @@
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function ()
-      vim.o.background = "dark"
-      vim.cmd([[colorscheme gruvbox]])
-    end
-  },
-  { "nvim-tree/nvim-web-devicons", lazy = true },
-  {
-  	'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-      options = {
-        theme = "auto"
-      }
-    },
-  },
-  { "folke/which-key.nvim", event = "VeryLazy" },
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			vim.o.background = "dark"
+			vim.cmd([[colorscheme gruvbox]])
+		end,
+	},
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			options = {
+				theme = "auto",
+			},
+		},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			delay = 600,
+			preset = "modern",
+		},
+	},
 }
