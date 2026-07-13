0ffde4b4a6ad9e15479ed1375a4be38cf9c5a4be
Ilshat Ismagilov <ilshat.ismagilov@reksoft.com>
2026-07-13 11:27:59 +0300

    learning

---
+++ nvim-new/lua/plugins/git.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"tpope/vim-fugitive",
+		cmd = {
+			"Git",
+			"G",
+			"Gdiffsplit",
+			"Gvdiffsplit",
+			"Gedit",
+			"Gwrite",
+			"Gread",
+			"Ggrep",
+			"GMove",
+			"GDelete",
+			"GBrowse",
+		},
+	},
+}
